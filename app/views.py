551f48ec30d5f8e8ec6861e385e4cf925677e5ad
Michal Duczynski <mduczek@gmail.com>
2015-04-25 14:47:02 +0200

    db proxy

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+import requests
+import json
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -17,3 +19,16 @@ def settings():
 @app.route("/about")
 def about():
     return render_template("index.html")
+
+@app.route('/db', methods=['POST'])
+def db():
+	db_path = "https://7ti5720l:p1lkxlff673oyj9z@yew-8681597.us-east-1.bonsai.io/leuvenhack"
+	data = request.get_data()
+	db_req = json.loads(data)
+	link = db_path+'/'+db_req['index']
+	if db_req['method'] == 'GET':
+		r = requests.get(link)
+		return r.text
+	elif db_req['method'] == 'POST':
+		r = requests.post(link, data=db_req['query'])
+		return r.text
